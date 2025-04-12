40853dbe466a4897c3117106f16d98b3f0a96756
Aleksandr Duplishchev <2ppalex@gmail.com>
2025-04-12 09:20:22 +0300

    configure GPIO for unused pins

--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 
 void main(void)
 {
+    // enable pull-up for unused pins: PA4, PC3, PC4
+    GPIOA->CR1 |= 1u << 4;
+    GPIOC->CR1 |= 1u << 3;
+    GPIOC->CR1 |= 1u << 4;
+
     while (1)
     {
     }
