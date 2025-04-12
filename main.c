6a96188ccf0a3458fdf45b5c3d3b5b73fbc24f1a
Aleksandr Duplishchev <2ppalex@gmail.com>
2025-04-12 13:04:46 +0300

    switch system clock source from HSI to HSE

--- main.c
+++ main.c
@@ -14,6 +14,26 @@ void main(void)
     GPIOA->DDR |= LED_ERR_MASK;
     GPIOA->DDR |= LED_OK_MASK;
 
+    // configure clock to 12 MHz fed from HSE
+    // set system clock presclaer to 1
+    CLK->CKDIVR = 0;
+    // indicate progress by turning LED_ERR on
+    GPIOA->ODR &= (uint8_t)(~LED_ERR_MASK);
+    // HSE selected as system clock source
+    CLK->SWR = 0x04;
+    // wait until clock ready
+    while (!(CLK->ECKCR & CLK_ECKCR_HSERDY))
+        ;
+    // indicate progress by turning LED_OK on
+    GPIOA->ODR &= (uint8_t)(~LED_OK_MASK);
+    // enable clock switch execution
+    CLK->SWCR |= CLK_SWCR_SWEN;
+    // wait until clock switched
+    while (CLK->SWCR & CLK_SWCR_SWBSY)
+        ;
+    // indicate progress by turning LED_ERR off
+    GPIOA->ODR |= LED_ERR_MASK;
+
     while (1)
     {
     }
